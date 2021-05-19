2e7c5ad70ad7c4f02e476706253b7c21037d0038
DodiDevrian <84231458+DodiDevrian@users.noreply.github.com>
2021-05-19 12:58:44 +0700

    Update login.py

--- frontend/login.py
+++ frontend/login.py
@@ -7,7 +7,6 @@ def insertUsernamePassword(cnx):
     username = input("username: ")
     password = getpass.getpass("Password: ")
 
-    # check auth akan menerima id, username, dan role
     dataAuth = auth.login(cnx, username, password)
     if dataAuth:
         menu.menu_admin(cnx, dataAuth)
